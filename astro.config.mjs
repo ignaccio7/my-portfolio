0da0474425d70290bfa8c72c8143f9bde62b939b
ignaccio7 <nacho260298@gmail.com>
2024-10-17 23:20:20 -0400

    Fourth commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ignaccio7.github.io',
+  base: 'main-directory',
   integrations: [tailwind()]
 });
